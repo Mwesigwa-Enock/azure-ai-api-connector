4798e038e3383ad3db3645af34833423b84c1692
enockmwesigwa <enockmwesigwa7@gmail.com>
2025-05-07 15:44:17 +0300

    Added logic for extraction of id back information

--- AzureCognitiveIntegration/Features/DocumentAnalysis/Models/NationalIdBackDetails.cs
+++ AzureCognitiveIntegration/Features/DocumentAnalysis/Models/NationalIdBackDetails.cs
@@ -5,5 +5,30 @@ namespace AzureCognitiveIntegration.Features.DocumentAnalysis.Models;
 /// </summary>
 public class NationalIdBackDetails
 {
+    /// <summary>
+    /// Village
+    /// </summary>
+    public string? Village { get; set; }
     
+    
+    /// <summary>
+    /// Parish
+    /// </summary>
+    public string? Parish { get; set; }
+    
+    /// <summary>
+    /// SubCounty
+    /// </summary>
+    public string? SubCounty { get; set; }
+    
+    
+    /// <summary>
+    /// County
+    /// </summary>
+    public string? County { get; set; }
+    
+    /// <summary>
+    /// District
+    /// </summary>
+    public string? District { get; set; }
 }
